2b02fa1c31ce6343d32fd96b8a3e5209cb1bc207
kwangs7243 <kwangs7243@gmail.com>
2026-04-28 14:49:17 +0900

    setup uv environment

--- static/js/query.js
+++ static/js/query.js
@@ -1,8 +1,32 @@
 document.querySelector("#btn_query")
     .addEventListener("click",async function() {
         const question_type = document.querySelector("#query").value;
+        if (question_type === "monthly_summary") {
+            const year = document.querySelector("#year").value;
+            const month = document.querySelector("#month").value;
+            const params = {
+                year,
+                month
+            }
+            const result = await requestQuery(question_type,params);
+            renderText(result);
+        }
         const result = await requestQuery(question_type);
         renderText(result);
+    });
+
+document.querySelector("#query")
+    .addEventListener("change" , function() {
+        const question_type = document.querySelector("#query").value;
+        const yearInput = document.querySelector("#year")
+        const monthInput = document.querySelector("#month")
+        if (question_type === "monthly_summary") {
+            yearInput.style.display = "inline-block";
+            monthInput.style.display = "inline-block";
+        }else {
+            yearInput.style.display = "none";
+            monthInput.style.display = "none";
+        }
     });
 
 
